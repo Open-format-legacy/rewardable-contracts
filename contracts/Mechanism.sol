b32fbdb1433d1ac817ec178c86dc70d80475b2db
Andy <andy.kitt@simpleweb.co.uk>
2022-02-28 09:03:21 +0000

    mechanism contract and interfaces

---
+++ contracts/Mechanism.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Mechanism is Ownable {
+    uint256 public rewardRate;
+
+    event RewardRateSet(uint256);
+
+    constructor(uint256 _rewardRate){
+        require(_rewardRate > 0, "Reward rate can't be 0");
+        rewardRate = _rewardRate;
+    }
+
+    function setRewardRate(uint256 _amount) public onlyOwner {
+        rewardRate = _amount;
+
+        emit RewardRateSet(_amount);
+    }
+
+    function calculateRewards(uint256 _currentRewards) external view returns (uint256) {
+        return _currentRewards += rewardRate;
+    }
+}
